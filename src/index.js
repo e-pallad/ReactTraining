b9fba90c55338d4a4f89b5e89fa28421e98e6983
Eric Pallad <er10092@mailingwork.local>
2020-07-08 15:01:17 +0200

    c06

--- src/index.js
+++ src/index.js
@@ -8,18 +8,34 @@ let bookList = [
   {"title": "Cats Cradle", "author": "Kurt Vonnegurt", "pages": 304}
 ]
 
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
   return (
     <section>
       <h2>{title}</h2>
       <p>by: {author}</p>
       <p>Pages: {pages} pages</p>
+      <p>Free Book Mark today: {freeBookmark ? 'yes!' : 'no!'}</p>
     </section>
   )
 }
 
+const Hiring = () => 
+  <div>
+    <p>The Library is hiring! Go to www.library.com/jobs for more.</p>
+  </div>
+
+const NotHiring = () => 
+  <div>
+    <p>The Library is not hiring! Check back later for more info.</p>
+  </div>
+
 class Library extends React.Component {
-  state = { open: false }
+  
+  state = { 
+    open: true, 
+    freeBookmark: false,
+    hiring: true
+  }
 
   toggleOpenClosed = () => {
     this.setState(prevState => ({
@@ -31,6 +47,7 @@ class Library extends React.Component {
     const { books } = this.props
     return (
       <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
         <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map(
@@ -40,6 +57,7 @@ class Library extends React.Component {
               title={book.title} 
               author={book.author} 
               pages={book.pages} 
+              freeBookmark={this.state.freeBookmark}
             />
         )}
       </div>
